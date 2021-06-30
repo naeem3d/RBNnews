653d521c20281f9c91fa8683c07f08d40e6a03c7
naeem3d@gmail.com <naeem3d@gmail.com>
2021-06-30 09:50:37 -0400

    break newsView controller to ViewModel for more management and easy to test

--- RBSNews/NewsViewController.swift
+++ RBSNews/NewsViewController.swift
@@ -10,48 +10,34 @@ import SafariServices
 
 class NewsViewController: UIViewController {
     
-    var newsViewModel: NewsViewModel?
-    var articles: [Article]?
+    var newsViewModel: NewsViewModel!
     @IBOutlet weak var newsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "News screen"
         newsViewModel = NewsViewModel()
-        newsViewModel?.parseJSON({ [weak self] (news) in
-			self?.articles = news?.articles
-			self?.newsTableView.reloadData()
-           
-          
+        newsViewModel?.parseJSON({ [weak self] in
+            self?.newsTableView.reloadData()
         })
     }
-    
 }
 
 extension NewsViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let articles = articles else {
-            return 0
-        }
-        return articles.count
+        return newsViewModel.numberOfArticles
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell", for: indexPath) as? NewsTableViewCell else {
             return UITableViewCell()
         }
-        if let article = articles?[indexPath.row] {
-            if let source = article.source,  let title = article.title {
-                let name = source.name
-                cell.setupNewsCell(source: name, title: title)
-                cell.textLabel?.numberOfLines = 0
-            }
-            if let imageUrl = article.urlToImage {
-                cell.setupNewsImage(imageURL: imageUrl)
-            }
-        }
         
+        let article = newsViewModel.getArticlesData(for: indexPath.row)
+        
+        cell.setupNewsCell(source: article.name, title: article.title)
+        cell.setupNewsImage(imageURL: article.imageUrl)
         return cell
     }
     
@@ -61,11 +47,12 @@ extension NewsViewController: UITableViewDataSource {
 extension NewsViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let article = articles?[indexPath.row], let urlString = article.url, let url = URL(string: urlString) {
-            let safariViewController = SFSafariViewController(url: url)
+        
+        let urlString = newsViewModel.getArticleUrl(for: indexPath.row)
+        let url = URL(string: urlString)
+        let safariViewController = SFSafariViewController(url: url!)
             present(safariViewController, animated: true, completion: nil)
             safariViewController.delegate = self
-        }
     }
 }
 
--- RBSNews/ViewModel/NewsViewModel.swift
+++ RBSNews/ViewModel/NewsViewModel.swift
@@ -8,21 +8,64 @@
 import UIKit
 
 class NewsViewModel {
+    
     static let shared = NewsViewModel()
     let cache = NSCache<NSString,UIImage>()
-    init() {}
+
     
-    func parseJSON(_ completion: @escaping (News?) -> Void) {
+    private var articles: [Article]?
+
+    
+    var numberOfArticles: Int {
+        return articles?.count ?? 0
+    }
+    
+    init() {}
+    func parseJSON(_ completion: @escaping () -> Void) {
         if let url = Bundle.main.url(forResource: "News", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: url)
                 let decoder = JSONDecoder()
                 let jsonData = try decoder.decode(News.self, from: data)
-                completion(jsonData)
+                
+                self.articles = jsonData.articles
+
+                completion()
             } catch {
                 print("error while decoding the JSON data")
-                completion(nil)
+                completion()
             }
         }
+    }
+    
+    
+    func getArticlesData(for row:Int)-> (title:String, imageUrl:String, name:String) {
+        
+        if let articles = articles {
+            let article = articles[row]
+            var name = ""
+            if let source = article.source {
+                name = source.name
+            }
+            let title = article.title ?? ""
+            let imageUrl = article.urlToImage ?? ""
+
+            return (title, imageUrl, name)
+        }
+        
+        return ("", "", "")
+        
+    }
+    
+    
+    func getArticleUrl(for row:Int)-> String {
+        
+        if let articles = articles {
+            let article = articles[row]
+            return article.url ?? ""
+        }
+        
+        return ""
+        
     }
 }
