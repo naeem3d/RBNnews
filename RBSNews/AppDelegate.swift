d72b4dd50323f0765d960a269380df7127622c41
MAC <mac@MACs-MacBook-Pro.local>
2021-07-04 07:04:09 +0530

    testing

--- RBSNews/AppDelegate.swift
+++ RBSNews/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print("I am first function called when user launch app ")
         return true
     }
 
